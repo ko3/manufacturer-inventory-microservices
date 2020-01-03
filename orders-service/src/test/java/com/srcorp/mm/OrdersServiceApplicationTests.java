a90e056fac28b5159e0ec73b6c625c4b297a47c1
ko3 <ek.lft15@gmail.com>
2020-01-02 18:39:22 -0600

    Initial

---
+++ orders-service/src/test/java/com/srcorp/mm/OrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srcorp.mm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rawmaterial-service/src/main/java/com/srcorp/mm/RawmaterialServiceApplication.java
@@ -0,0 +1,13 @@
+package com.srcorp.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawmaterialServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RawmaterialServiceApplication.class, args);
+	}
+
+}
